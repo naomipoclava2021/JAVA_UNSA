48b0bc29739c6e824803a555c172cfc9c4c0cc5a
naomipoclava2021 <milagrito1dediciembre@gmail.com>
2022-03-25 13:05:11 -0300

    cambios 25/03

--- Practico Clase Nº 2/src/controlador/Main1.java
+++ Practico Clase Nº 2/src/controlador/Main1.java
@@ -3,6 +3,64 @@ package controlador;
 public class Main1 {
 
 	public static void main(String[] args) {
+		
+		//EJERCIO 1
+		//Crea un programa en Java en donde se muestre la estructura basica del mismo
+		
+		
+		//EJERCIO 2
+		//Crea un program en donde se muestren los diferentes tipos de comentarios que hay en Java
+		
+		
+		//EJERCIO 3
+		//Crea un programa en donde se declaren, al menos 3 identificadores validos y 3 invalidos,
+		//a estos poner comentarios el porque son incorrectas.
+		
+		//EJERCICIO 4
+		//Declarar 5 variables de cada tipo de datos que utiliza Java. Dar formato al codigo mediante comentarios.
+		
+		//EJERCICIO 5
+		//Realizar un programa en donde se muestren los diferentes operadores que existen en Java.
+		//Utilizar bloques de comentarios para estructurar el mismo.
+		
+		//EJERCICIO 6
+		//Utilizar el tipo de dato entero para crear al menos  5 variables con numeros octales y hexadecimales.
+		
+		
+		//EJERCICIO 7
+		//Declarar variables de tipo cadena de caracteres y mostrarlas por pantalla.
+		
+		//EJERCICIO 8
+		//Realizar un programa que realice las operaciones basicas de suma, resta, producto y division de dos numeros.
+		//Hacer con numeros enteros, y decimales.
+		
+		
+		//EJERCICIO 9
+		//Siguiendo el ejercicio anterior, mezclar sumas con numeros enteros y decimales, que sucede?
+		//Describal el resultado mediante comentarios.
+		
+		//EJERCICIO 10
+		//Muestre en un ejemplo como se usan los operadores incremento y decremento.
+		
+		//EJERCICIO 11
+		//Realizar un programa que muestre el uso del operador racional. Pruebe al menos 5 ejemplos.
+		
+		//EJERCICIO 12
+		//Realizar un programa que muestre el uso del operador logico. Pruebe al menos 5 ejemplos.
+		
+		//EJERCICIO 13
+		//Realizar un programa que calcule el resultado de una expresion algebraica, use distintos operadores.
+		//Realizar al menos 3 expresiones algebraicas.
+		
+		//EJERCICIO 14
+		//Conversion de tipos. Realizar un programa que muestre la conversion de tipo uppcasting y downcating.
+		//Usar tipos enteros y decimales.
+		
+		//EJERCICIO 15
+		//Sera posible almacenar un tipo de dato char en un tipo entero? De ser posible explique con comentarios el porque.
+		
+		
+		
 		// TODO Auto-generated method stub
 		/*este es un comentario
 		 *varios comentario
