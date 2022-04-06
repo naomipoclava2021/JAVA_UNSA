7f77a94341c104992c14d5bd985cd3c758366980
naomipoclava2021 <milagrito1dediciembre@gmail.com>
2022-04-06 11:44:19 -0300

    arrays

--- Practico Clase Nº 5 y Nº6/src/controlar/Ejercicio1.java
+++ Practico Clase Nº 5 y Nº6/src/controlar/Ejercicio1.java
@@ -1,5 +1,7 @@
 package controlar;
 
+import java.util.Scanner;
+
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
@@ -8,8 +10,30 @@ public class Ejercicio1 {
 		 * Ejercicio1 Crea un array de 10 numeros enteros, con valores solicitados por
 		 * teclado. Mostrar por consola en indice y el valor de cada elemento. Realizar
 		 * dos metodos, uno para el ingreso de datos y otro para la salida de datos.
+		 * 
+		 * 
 		 */
 
+		int[] vector = new int[10];
+		cargar(vector);
+		mostrar(vector);
+
+	}
+
+	public static void cargar(int[] vector) {
+		Scanner ingreso = new Scanner(System.in);
+		for (int i = 0; i < 10; i++) {
+			System.out.println("ingrese el numero   " + (i + 1) + ": ");
+			vector[i] = ingreso.nextInt();
+		}
+		System.out.println("Vector carcada");
+		ingreso.close();
+	}
+
+	public static void mostrar(int[] vector) {
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i + vector[i]);
+		}
 	}
 
 }
--- Practico Clase Nº 5 y Nº6/src/controlar/Ejercicio4.java
+++ Practico Clase Nº 5 y Nº6/src/controlar/Ejercicio4.java
@@ -8,6 +8,29 @@ public class Ejercicio4 {
 		 * Ejercicio4 Generar 20 numeros enteros entre 1 y 100 de forma aleatoria y
 		 * contar cuantos de esos numeros enteros son divisibles entre 5.
 		 */
+		int vector[] = new int[10];
+
+		for (int i = 0; i < vector.length; i++) {
+			System.out.println(vector[i] + " - ");
+		}
+		System.out.println();
+		for (int i = 0; i < vector.length; i++) {
+			int num = (int) (Math.random() * 100);
+			vector[i] = num;
+		}
+
+		for (int i = 0; i < vector.length; i++) {
+			System.out.println(vector[i] + " - ");
+		}
+		System.out.println();
+
+		int contador = 0;
+		for (int i = 0; i < vector.length; i++) {
+			if (vector[i] % 5 == 0) {
+				contador++;
+			}
+		}
+		System.out.println("En tu lista tienes: " + contador + "numeros divisibles por 5");
 
 	}
 
