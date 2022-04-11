c5733598117df6001f6e1bad37ec299f1e3ea399
naomipoclava2021 <milagrito1dediciembre@gmail.com>
2022-04-11 16:57:50 -0300

    se agrego el main

--- Modulo 2 Clase N°1 y N°2/src/poo/Main.java
+++ Modulo 2 Clase N°1 y N°2/src/poo/Main.java
@@ -4,6 +4,56 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		/*
+		 * Trabajo Pactico N°1 Modulo 2 - Clase1
+		 * Creacion de Clase y Objetos. Sintaxis y variables
+		 */
+		
+		/*
+		 * Ejercicio N°1
+		 * Crear una clase llamada Persona con los atributos: Nombre, Apellido, DNI, Domicilio.
+		 * Con los metodos: Constructor y un metodo que permita mostrar la informacion de la persona.
+		 */
+		
+		/*
+		 * Ejercicio N°2
+		 * Crear una clase llamanda serie con los atributos: titulo, numeros de temporadas, genero y creador. Con los siguientes
+		 * metodos: Constructor, mostrar todos los atributos, sobrescribir todos los atributos.
+		 * 
+		 */
+		
+		/*
+		 * Ejercicio N°3
+		 * Crear una clase NumerosEnteros y redefinir las operaciones elementales (+,-,*,/). Esta clase tendra los metodos suma(),
+		 * resta(), multiplicacion(), y division() que recibira como parametros otro objeto de la misma clase NumerosEnteros.
+		 */
+		
+		/*
+		 * Ejercicio N°4
+		 * Crear una clase llamada Contador que contenga un unico atributo entero llamado cont. La clase tendra los siguientes
+		 * constructores: Constructor por defecto, Constructodr con parametros para inicializar el contador con un valor no 
+		 * negativo. Si el valor inicial que se recibe es negativo el contador tomara el valor cero como valor inicial. Constructor 
+		 * copia. Ademas de los metodos getter y setter, la clase contendra  los metodos: incrementar: incrementa el contador en 
+		 * una unidad. El contador nunca podra tener un valor negetivo. Si al decrementar  se alcanza un valor negativo el contador
+		 * toma el valor cero.
+		 */
+		
+		/*
+		 * Ejercicio N°5
+		 * Escribe un clase Cuenta para representar una cuenta bancaria. Los datos de la cuenta son: nombre del cliente (String),
+		 * numero de cuenta (String), tipo de interes (double) y saldo(double). La clase contendra los siguientes metodos:
+		 * Constructor por defecto y Constructor con todos los parametros Constructor copia. Metodos setters/getters para
+		 * asignar y obtener los datos de la cuenta. Metodos ingreso y reintegro. Un ingreso consiste en aumentar el saldo
+		 * en la cantidad que se indique.
+		 * Esa cantidad no puede ser negativa. Un reintegro consiste en diminuir el saldo pero antes de debe comprobar que 
+		 * hay saldo suficiente. La cantidad no puede ser negativa. Los metodos de ingreso y reintegro devulve true si la operacion
+		 * se ha podido realizar o false en caso contrario. Metodos de transferencia que permita pasar dinero de una cuenta a
+		 * otra siempre que en la cuenta de origen haya dinero suficiente para poder hacerla. Ejemplo de uso del metodo
+		 * de trasferencia: 
+		 * CuentaOrigen.transferencia(CuentaDestino, importe); que indica que queremos hacer una transferencia desde la cuentaOrigen
+		 * a CuentaDestino del importe indicado.
+		 */
 
 	}
 
