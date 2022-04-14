0278db06eb23f3ca3746d19906ade92cc8493a09
naomipoclava2021 <milagrito1dediciembre@gmail.com>
2022-04-14 15:28:30 -0300

    se agrega un readme

--- Practico Clase Nº 2/src/controlador/Ejercicio15.java
+++ Practico Clase Nº 2/src/controlador/Ejercicio15.java
@@ -7,9 +7,9 @@ public class Ejercicio15 {
 		// EJERCICIO 15
 		// Sera posible almacenar un tipo de dato char en un tipo entero? De ser posible
 		// explique con comentarios el porque.
-		char sePuedeAlmacenarEntero = 23;
-		System.out.println(sePuedeAlmacenarEntero);
-		// si se puede
+		int tipoEntero = '#';
+		System.out.println(tipoEntero);
+		// si se puede te devulven valores numericos
 
 	}
 
