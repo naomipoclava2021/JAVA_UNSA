59b0ac9bd11572e9cb649d473a51f42dd78a9321
naomipoclava2021 <milagrito1dediciembre@gmail.com>
2022-04-14 18:36:43 -0300

    Modulo 1

---
+++ Practico Clase Nº 2/src/controlador/Practica.java
@@ -0,0 +1,10 @@
+package controlador;
+
+public class Practica {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Practico Clase Nº 5 y Nº6/src/controlar/Practica.java
@@ -0,0 +1,10 @@
+package controlar;
+
+public class Practica {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Modulo 1");
+	}
+
+}
